ba4cc8318b905cd467736c2552988e8f3dd84c4a
shubhambhamare <shubhambhamre632@gmail.com>
2022-02-17 12:07:38 +0530

    first commit

---
+++ src/component/Notes.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import img1 from './WhatsApp Image 2022-02-17 at 11.55.16 AM.jpeg'
+import img2 from "./WhatsApp Image 2022-02-17 at 11.55.17 AM (1).jpeg"
+import img3 from './WhatsApp Image 2022-02-17 at 11.55.17 AM (2).jpeg'
+import img4 from './WhatsApp Image 2022-02-17 at 11.55.17 AM.jpeg'
+
+
+function Notes() {
+    return(
+        <div>
+            <h3>Library v/s FrameWork (&) CRA Structure
+
+            </h3><br />
+            <img src={img1} alt="" />
+            <img src={img2} alt="" />
+            <img src={img3} alt="" />
+            <img src={img4} alt="" />
+        </div>
+    )
+    
+}
+export default Notes;
